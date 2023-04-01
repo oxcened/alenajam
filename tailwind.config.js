3f1c46d991945a6a9e2290699f5ccadd4a0ffcd3
Alen Ajam <alen.ajam@live.com>
2023-04-01 19:14:46 +0200

    changed text, removed hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      keyframes: {
-        'fade-in': {
-          from: { opacity: 0 },
-          to: { opacity: 1 },
-        }
-      },
-      animation: {
-        'fade-in-t1': 'fade-in 1s ease-in-out .5s both',
-        'fade-in-t2': 'fade-in 1s ease-in-out 2s both',
-        'fade-in-t3': 'fade-in 1s ease-in-out 5s both',
-        'fade-in-t4': 'fade-in 1s ease-in-out 7s both'
+      listStyleType: {
+        square: 'square',
       }
     },
   },
